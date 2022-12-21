d706bf9930818e6d7e9fffe9858dce9f5a994576
xiaoyulejia <52663365+xiaoyulejia@users.noreply.github.com>
2022-12-21 12:49:03 +0800

    1

--- cout.cpp
+++ cout.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
+int b = 7;
 int main()
 {
     float a = 3.1415926;
-    cout << fixed << setprecision(2) << a;
+    cout << fixed << setprecision(2) << a << endl;
+    cout << b << endl;
     return 0;
 }
